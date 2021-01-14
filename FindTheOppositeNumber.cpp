acde5db31cf2542756c16874c1a963db2c684e64
Andrew Virya Victorio <andrew.avv03@gmail.com>
2021-01-14 21:25:09 +0700

    Add files via upload

---
+++ FindTheOppositeNumber.cpp
@@ -0,0 +1,21 @@
+/*
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+*/
+#include <iostream>
+using namespace std;
+
+int opposite (int number) {
+    return -number;
+}
+
+int main() {
+    cout << opposite(-1) << endl;
+}
+
+//DUPLICATE
